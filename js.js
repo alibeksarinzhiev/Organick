cd1beba47291f84951cc35aa437d4874100ac533
alibek <alibek1024@mail.ru>
2023-10-31 21:02:55 +0600

    31.10

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
+import {addCart} from "./pages/cart/cart.js";
+let cart = []
+
+
+
 let cards = document.querySelector('.products__cards')
 let showBtn = document.querySelector('.products__btnbox button')
-
+let btncart = document.querySelector('.cart')
+let cartshow = document.querySelector('.cartshow')
+let closecart = document.querySelector('.cartshow h1')
 
 let count = 8
 showBtn.addEventListener('click',()=>{
@@ -36,11 +43,31 @@ let getproducts = ()=>{
             <h2>${el.title}</h2>
             <div class="card__bottom">
             <p><span class="old__price"><s>$20.00</s></span> $${el.price}.00 USD</p>
-            
             </div>
+            <button data-id=${el.id} class="addcart">Купить</button>
             </div>
                 `
+
             })
+            let addbtncart = document.querySelectorAll('.addcart')
+            Array.from(addbtncart).forEach((item)=>{
+                item.addEventListener('click',()=>{
+                   cart = [...cart,json.find((one)=>{
+                       return one.id === +item.dataset.id
+                   })]
+                    console.log(cart)
+
+                })
+            })
+
         })
 }
 getproducts()
+btncart.addEventListener('click',()=>{
+cartshow.classList.toggle('active')
+})
+closecart.addEventListener('click',()=>{
+    cartshow.classList.toggle('active')
+})
+
+
