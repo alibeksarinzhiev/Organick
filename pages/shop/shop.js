43651d5aac9aefd51a08ab6df7099fe15576b6af
invictus1902 <131914647+invictus1902@users.noreply.github.com>
2023-10-31 08:48:32 +0600

    emir 31.10.23 pages +service +404 not found

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -2,30 +2,31 @@ let cardShop = document.querySelector('.products__shop__card')
 let btnCategory = document.querySelectorAll('.shop__category li')
 
 let status = 'All'
-btnCategory.forEach((el)=>{
-    el.addEventListener('click',()=>{
+btnCategory.forEach((el) => {
+    el.addEventListener('click', () => {
         status = el.textContent
         getProductsShop()
     })
 })
 
-let getProductsShop = ()=>{
+let getProductsShop = () => {
     cardShop.innerHTML = ''
     fetch('http://localhost:3000/products')
-        .then((res)=>res.json())
-        .then((json)=>{
-            json.filter((el)=>{
-                if (status === 'All'){
+        .then((res) => res.json())
+        .then((json) => {
+            json.filter((el) => {
+                if (status === 'All') {
+                    return el
+                } else if (status === el.category) {
                     return el
                 }
-                else if (status === el.category){
-                    return  el
-                }
-            }).forEach((el)=>{
-                cardShop.innerHTML+=`
+            }).forEach((el) => {
+                cardShop.innerHTML += `
                 <div class="card">
             <button>${el.category}</button>
-            <img src="../../${el.image}" alt="">
+            <a href="../single/single.html#${el.id}">          
+              <img src="../../${el.image}" alt="">
+              </a>
             <h2>${el.title}</h2>
             <div class="card__bottom">
             <p><span class="old__price"><s>$20.00</s></span> $${el.price}.00 USD</p>
