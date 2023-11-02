40d0ec598a33620c6ce3cead1d2d9ba35bb9d106
adilet <adik.it22run@gmail.com>
2023-11-02 21:21:18 +0600

    adilet 02-11

---
+++ pages/single/single.js
@@ -0,0 +1,23 @@
+let card = document.querySelector('.one__product')
+
+fetch(`http://localhost:3000/products/${location.hash.slice(1)}`)
+    .then((res)=>res.json())
+    .then((json)=>{
+        card.innerHTML +=`
+   <div class="box__img">
+                    <img src="${json.image}" alt="">
+                </div>
+                <div class="right__box">
+                    <div class="right__top">
+                        <h2>${json.title}</h2>
+                        <h3><s class="pay">$20.00</s> $${json.price}.00 USD</h3>
+                        <p>${json.description}</p>
+                    </div>
+                    <div class="right__bottom">
+                        <h3>Quantity :</h3>
+                        <button class="1button">1</button>
+                        <button class="2button">Add To Cart</button>
+                    </div>
+                </div>
+    `
+    })
